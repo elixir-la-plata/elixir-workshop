acf0cf63816a398304283ed65a9da4a10e18d309
Nicanor Perera <nicanorperera@gmail.com>
2020-02-15 14:51:13 -0300

    Add plug. Add comments to show.

---
+++ lib/yo_web/plugs/unauthorized.ex
@@ -0,0 +1,13 @@
+defmodule YoWeb.Plugs.Unauthorized do
+  import Plug.Conn
+
+  def init(default), do: default
+
+  def call(conn, options) do
+    message = options[:message] || "No autorizado"
+
+    conn
+    |> Plug.Conn.resp(401, message)
+    |> halt()
+  end
+end
